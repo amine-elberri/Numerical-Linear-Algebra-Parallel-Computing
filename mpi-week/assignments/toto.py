0c51efb20f52fbdbd9383badc3adb919b3e3c972
Amine EL BERRI <113511305+amine-elberri@users.noreply.github.com>
2023-04-24 05:15:29 +0200

    done

---
+++ mpi-week/assignments/toto.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from mpi4py import MPI
+
+COMM = MPI.COMM_WORLD
+RANK= COMM.Get_rank()
+SIZE = COMM.Get_size()
+
+def f1():
+    print("toto",RANK,SIZE)
+    
+f1()
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+
